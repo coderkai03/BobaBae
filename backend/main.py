b1ac03090022a3e6c2646c04598e2d756ef5465f
Bill Zhang <billzhangsc@gmail.com>
2024-05-18 23:41:49 -0700

    Finish

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,7 @@ load_dotenv()
 app = Flask(__name__)
 CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}})  # Allow requests from localhost:5173
 
-NEXT_PUBLIC_GOOGLE_API = os.getenv('NEXT_PUBLIC_GOOGLE_API')
+GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
 
 @app.route('/api/places', methods=['GET'])
 def get_places():
@@ -19,7 +19,7 @@ def get_places():
     rankPreference = request.args.get('rankPreference')
     type_place = request.args.get('type')
 
-    url = f'https://maps.googleapis.com/maps/api/place/nearbysearch/json?keyword={keyword}&location={location}&radius={radius}&type={type_place}&rankPreference={rankPreference}&key={NEXT_PUBLIC_GOOGLE_API}'
+    url = f'https://maps.googleapis.com/maps/api/place/nearbysearch/json?keyword={keyword}&location={location}&radius={radius}&type={type_place}&rankPreference={rankPreference}&key={GOOGLE_API_KEY}'
 
     try:
         response = requests.get(url)
@@ -31,7 +31,7 @@ def get_places():
 @app.route('/api/name', methods=['GET'])
 def get_name():
     place_id = request.args.get('place_id')
-    url = f'https://maps.googleapis.com/maps/api/place/details/json?place_id={place_id}&fields=name,formatted_address,geometry&key={NEXT_PUBLIC_GOOGLE_API}'
+    url = f'https://maps.googleapis.com/maps/api/place/details/json?place_id={place_id}&fields=name,formatted_address,geometry&key={GOOGLE_API_KEY}'
     
     print(url)
     try:
