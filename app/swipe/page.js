602a9be348f637a25d3700041cdbd9b7b7bc4e6a
Bill Zhang <billzhangsc@gmail.com>
2024-05-18 23:20:30 -0700

    Fix merge conflict

--- app/swipe/page.js
+++ app/swipe/page.js
@@ -1,73 +1,88 @@
-"use client"
-import { Box, Stack, Typography, Button } from '@mui/material';
-import { useUser, useAuth } from '@clerk/clerk-react';
-import { useState, useEffect } from 'react';
-import { updateDoc, doc, collection, getDocs, arrayUnion } from 'firebase/firestore';
-import { db } from '@/firebaseConfig';
+'use client'
+import {Box, Stack, Typography, Button} from '@mui/material'
+import {useUser, useAuth} from '@clerk/clerk-react'
+import {useState, useEffect} from 'react'
+import {
+  updateDoc,
+  doc,
+  collection,
+  getDocs,
+  arrayUnion,
+} from 'firebase/firestore'
+import {db} from '@/firebaseConfig'
 
 export default function Swipe() {
-  const { user, isLoading } = useUser();
-  const { signOut } = useAuth();
-  const [usersData, setUsersData] = useState([]);
-  const [currentIndex, setCurrentIndex] = useState(0);
+  const {user, isLoading} = useUser()
+  const {signOut} = useAuth()
+  const [usersData, setUsersData] = useState([])
+  const [currentIndex, setCurrentIndex] = useState(0)
   console.log(user)
 
   const fetchUsers = async () => {
-    const usersCollection = collection(db, 'users');
-    const usersSnapshot = await getDocs(usersCollection);
-    const usersList = usersSnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-    
+    const usersCollection = collection(db, 'users')
+    const usersSnapshot = await getDocs(usersCollection)
+    const usersList = usersSnapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }))
+
     // Filter out the current user
-    const filteredUsers = usersList.filter(userData => userData.id !== user.id).sort(() => Math.random() - 0.5);
-    console.log(filteredUsers);
-    
-    setUsersData(filteredUsers);
-  };
+    const filteredUsers = usersList
+      .filter((userData) => userData.id !== user.id)
+      .sort(() => Math.random() - 0.5)
+    console.log(filteredUsers)
+
+    setUsersData(filteredUsers)
+  }
 
   useEffect(() => {
     if (!isLoading && user) {
-      fetchUsers();
+      fetchUsers()
     }
-  }, [user, isLoading]);
+  }, [user, isLoading])
 
   const handleNextUser = () => {
-    console.log('Skipped: ', currentUser);
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % usersData.length);
-  };
+    console.log('Skipped: ', currentUser)
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % usersData.length)
+  }
 
-  const handleMatch = async () => { 
-    console.log('Matched with: ', currentUser);
-    const matchedUser = usersData[currentIndex];
-    const matchedUserId = doc(db, 'users', matchedUser.id);
-    
+  const handleMatch = async () => {
+    console.log('Matched with: ', currentUser)
+    const matchedUser = usersData[currentIndex]
+    const matchedUserId = doc(db, 'users', matchedUser.id)
+
     // Add matchedUserId to the current user's matches array in Firebase
-    const userRef = doc(db, 'users', user.id);
+    const userRef = doc(db, 'users', user.id)
     try {
-      await updateDoc(userRef, {
-        matches: arrayUnion(matchedUserId)
-      }, { merge: true }); // Add { merge: true } to merge the new data with existing fields
-      console.log('Document update successful');
+      await updateDoc(
+        userRef,
+        {
+          matches: arrayUnion(matchedUserId),
+        },
+        {merge: true},
+      ) // Add { merge: true } to merge the new data with existing fields
+      console.log('Document update successful')
     } catch (error) {
-      console.log('Document update failed:', error);
+      console.log('Document update failed:', error)
     }
-    
-    handleNextUser();
+
+    handleNextUser()
 
     // Log the user's matches
-    const userMatches = user.matches || [];
-    console.log('User matches:', userMatches);
-  };
+    const userMatches = user.matches || []
+    console.log('User matches:', userMatches)
+  }
 
   // Render loading state while user data is being fetched
   if (isLoading) {
-    return <div>Loading...</div>;
+    return <div>Loading...</div>
   }
 
   if (usersData.length === 0) {
-    return <div>No users found</div>;
+    return <div>No users found</div>
   }
 
-  const currentUser = usersData[currentIndex];
+  const currentUser = usersData[currentIndex]
 
   return (
     <Box
@@ -88,20 +103,22 @@ export default function Swipe() {
         justifyContent="flex-end"
         paddingRight={5}
       >
-        <Button onClick={() => signOut()} variant="contained2" color="primary"
+        <Button
+          onClick={() => signOut()}
+          variant="contained2"
+          color="primary"
           sx={{
             backgroundColor: '#2b0303',
             transition: 'background-color 0.3s ease',
             color: 'white',
-            '&:hover':{
+            '&:hover': {
               backgroundColor: '#975629',
             },
           }}
         >
           Sign Out
         </Button>
-      
-
+      </Box>
       <Stack
         spacing={2}
         direction="column"
@@ -109,95 +126,105 @@ export default function Swipe() {
         justifyContent="center"
         flexGrow={1}
       >
-        <Typography variant="h2" color="black" paddingBottom="40px">  
+        <Typography variant="h2" color="black" paddingBottom="40px">
           Let's Swipe, {user.firstName}
         </Typography>
 
-        <Stack direction="row" alignItems="center" justifyContent="center" spacing={20}>
+        <Stack
+          direction="row"
+          alignItems="center"
+          justifyContent="center"
+          spacing={20}
+        >
           {/* Left Image */}
           <Box>
-            <img 
-              src="/xmark.png" 
-              onClick={handleNextUser} 
-              alt="pass" 
-              style={{ 
-                width: '100px', 
+            <img
+              src="/xmark.png"
+              onClick={handleNextUser}
+              alt="pass"
+              style={{
+                width: '100px',
                 height: '100px',
                 margin: '0 50px 0 0',
                 transition: 'transform 0.3s ease',
-                }} 
+              }}
               className="hovered-image"
             />
           </Box>
 
           {/* Profile Stack */}
-          {currentUser &&(<Stack
-            direction="column"
-            spacing={2}
-            alignItems="center"
-            justifyContent="center"
-            bgcolor={'white'}
-            borderRadius={4}
-            padding={2}
-            marginX={2}
-            width={400}
-            height={500}
-            sx={{
-              boxShadow: '0 0 40px rgba(0, 0, 0, 25%)',
-            }}
-          >
-
-            <div style={{width: '100%', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-              <img
-                src="/bobabae.png"
-                alt="pass"
+          {currentUser && (
+            <Stack
+              direction="column"
+              spacing={2}
+              alignItems="center"
+              justifyContent="center"
+              bgcolor={'white'}
+              borderRadius={4}
+              padding={2}
+              marginX={2}
+              width={400}
+              height={500}
+              sx={{
+                boxShadow: '0 0 40px rgba(0, 0, 0, 25%)',
+              }}
+            >
+              <div
                 style={{
-                  width: '250px',
-                  height: 'auto',
+                  width: '100%',
+                  height: '100vh',
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
                 }}
-              />
-            </div>
-            
-            {currentUser && (
-              <Box
-                key={currentUser.id}
-                display="flex"
-                flexDirection="column"
-                justifyItems={'flex-start'}
               >
-                <Typography variant="h3" color="black">
-                  {currentUser.name}
-                </Typography>
-                <Typography variant="h7" color="black">
-                  Age: {currentUser.age}
-                </Typography>
-                <Typography variant="h7" color="black">
-                  School: {currentUser.school}
-                </Typography>
-                {/* <Typography variant="h7" color="black">
-                  {currentUser.dietaryTags}
-                </Typography> */}
-              </Box>
-            )}
-            {/* <Button onClick={handleNextUser}>Next User</Button> */}
-          </Stack>
+                <img
+                  src={currentUser.photo}
+                  alt="pass"
+                  style={{
+                    width: '250px',
+                    height: 'auto',
+                  }}
+                />
+              </div>
+
+              {currentUser && (
+                <Box
+                  key={currentUser.id}
+                  display="flex"
+                  flexDirection="column"
+                  justifyItems={'flex-start'}
+                >
+                  <Typography variant="h3" color="black">
+                    {currentUser.name}
+                  </Typography>
+                  <Typography variant="h7" color="black">
+                    Age: {currentUser.age}
+                  </Typography>
+                  <Typography variant="h7" color="black">
+                    School: {currentUser.school}
+                  </Typography>
+                </Box>
+              )}
+            </Stack>
+          )}
 
           {/* Right Image */}
           <Box>
-            <img 
-              src="/checkmark.png" 
-              onClick={handleMatch} 
-              alt="match" 
-              style={{ 
-                width: '100px', 
+            <img
+              src="/checkmark.png"
+              onClick={handleMatch}
+              alt="match"
+              style={{
+                width: '100px',
                 height: '100px',
                 margin: '0 0 0 50px',
                 transition: 'transform 0.3s ease',
-                }} 
+              }}
               className="hovered-image"
             />
           </Box>
-        </Box>
+        </Stack>
       </Stack>
 
       {/* Menu Buttons */}
@@ -207,69 +234,72 @@ export default function Swipe() {
         alignItems="center"
         justifyContent="center"
         padding={2}
-        margin = {5}
+        margin={5}
       >
-        <Button 
-          href='/bucket' 
-          variant="contained" 
-          color="primary" 
-          sx={{ 
-            borderRadius: '50%', 
-            textAlign: 'center',
-            width: '100px',
-            height: '100px',
-            minwidth: 'auto',
-            backgroundColor: '#975629',
-            '&:hover':{
-              backgroundColor: '#2b0303',
-            },
-            }}
-        >
-          Baes
-        </Button>
         <Button
-          href='/swipe' 
-          variant="contained" 
-          color="primary" 
+          href="/bucket"
+          variant="contained"
+          color="primary"
           sx={{
-            borderRadius: '50%', 
+            borderRadius: '50%',
             textAlign: 'center',
             width: '100px',
             height: '100px',
             minwidth: 'auto',
             backgroundColor: '#975629',
-            '&:hover':{
+            '&:hover': {
               backgroundColor: '#2b0303',
             },
           }}
         >
-          lets<br/>swipe
+          Baes
         </Button>
-        <Button 
-          href='/profile' 
-          variant='contained' 
-          color="primary" 
-          sx={{ 
-            borderRadius: '50%', 
+        <Button
+          href="/swipe"
+          variant="contained"
+          color="primary"
+          sx={{
+            borderRadius: '50%',
             textAlign: 'center',
             width: '100px',
             height: '100px',
             minwidth: 'auto',
             backgroundColor: '#975629',
-            '&:hover':{
+            '&:hover': {
               backgroundColor: '#2b0303',
-            }, 
-            }}
+            },
+          }}
+        >
+          lets
+          <br />
+          swipe
+        </Button>
+        <Button
+          href="/profile"
+          variant="contained"
+          color="primary"
+          sx={{
+            borderRadius: '50%',
+            textAlign: 'center',
+            width: '100px',
+            height: '100px',
+            minwidth: 'auto',
+            backgroundColor: '#975629',
+            '&:hover': {
+              backgroundColor: '#2b0303',
+            },
+          }}
         >
           Profile
-        </MenuButton>
+        </Button>
       </Stack>
     </Box>
-  );
+  )
 }
 
-const MenuButton = ({ href, children }) => (
-  <Button href
+const MenuButton = ({href, children}) => (
+  <Button
+    href
     variant="contained"
     color="primary"
     sx={{
@@ -281,4 +311,4 @@ const MenuButton = ({ href, children }) => (
   >
     {children}
   </Button>
-);
+)
